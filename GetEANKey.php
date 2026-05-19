78860e47e9955c150fdb401c5590314c71acf735
VincentPapin <61055668+VincentPapin@users.noreply.github.com>
2026-05-19 12:06:41 +0200

    Refactor getEANKey function for clarity

--- GetEANKey.php
+++ GetEANKey.php
@@ -12,10 +12,10 @@ function getEANKey($EAN)
 		//Calcul de la somme, l'indice commence à 0 et non à 1 WinDev
 		$somme = $EAN[0]+ $EAN[1]*3+$EAN[2]+$EAN[3]*3+$EAN[4]+$EAN[5]*3+$EAN[6]+$EAN[7]*3+$EAN[8]+$EAN[9]*3+$EAN[10]+$EAN[11]*3;
 		
-		//division de la somme par 10 et calcul dur reste de la divison
+		//division de la somme par 10 et calcul du reste de la divison
 		$resteDivision = fmod($somme, 10);
 		
-		//Si le reste de la divison est différente de 0 alors 10 - reste de la divison
+		//Si le reste de la divison est différent de 0 alors 10 - reste de la divison
 		if($resteDivision != 0)
 		{
 			$key = 10-$resteDivision;
